bf870a70405b62f5ea83d02d863d59e98bf08e01
yumeng05 <yumeng05@baidu.com>
2020-09-15 14:08:18 +0800

    flutte_demo

---
+++ lib/pages/loading.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:world_time_app/api/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'London', flag: 'london.png', url: 'Europe/London');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue[900],
+        body: Center(
+            child: SpinKitFoldingCube(
+          color: Colors.white,
+          size: 50.0,
+        )));
+  }
+}
